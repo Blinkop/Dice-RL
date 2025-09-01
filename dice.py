18f17f712ce6ebf686f9c1c120774182a384a6ae
Anton Lysenko <anthony@Antons-Laptop.local>
2025-09-01 13:11:53 +0300

    added experiment progress saving

---
+++ dice.py
@@ -0,0 +1,393 @@
+from typing import Optional, Callable, List
+from abc import ABC
+from pathlib import Path
+
+import json
+
+from tqdm import tqdm
+
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam, SGD
+from torch.nn.utils import clip_grad_norm_
+
+from sklearn.utils import check_scalar
+
+from functions import StateActionNetwork
+from functions import DiceFunctions
+from utils import check_array, get_lambda_code
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+class Dice(ABC):
+    def __init__(
+        self,
+        q_function: StateActionNetwork,
+        w_function: StateActionNetwork,
+        gamma: float = 0.99,
+        q_lr: float = 1e-4,
+        w_lr: float = 1e-4,
+        lambda_lr: float = 1e-4,
+        f1_function: Callable[[torch.Tensor], torch.Tensor] = DiceFunctions.SCOPE_RL_P_2,
+        f2_function: Callable[[torch.Tensor], torch.Tensor] = DiceFunctions.CHI_SQUARED,
+        method_name: str = 'dual_dice',
+        alpha_q: Optional[float] = None,
+        alpha_w: Optional[float] = None,
+        alpha_r: Optional[float] = None,
+        w_positivity: Optional[bool] = None,
+        enable_lambda: Optional[bool] = None,
+        seed: int = None,
+        device: str = 'cpu'
+    ) -> None:
+        super().__init__()
+
+        check_scalar(
+            gamma, name='gamma', target_type=float, min_val=0.0, max_val=1.0
+        )
+        check_scalar(q_lr, name='q_lr', target_type=float, min_val=0.0)
+        check_scalar(w_lr, name='w_lr', target_type=float, min_val=0.0)
+        check_scalar(lambda_lr, name='lambda_lr', target_type=float, min_val=0.0)
+
+        self._seed = seed
+        self._device = device
+
+        self._torch_generator = torch.Generator()
+        if self._seed is not None:
+            self._torch_generator.manual_seed(self._seed)
+        else:
+            self._torch_generator.seed()
+
+        if method_name not in [
+            'dual_dice',
+            'gen_dice',
+            'gradient_dice',
+            'mql',
+            'mwl',
+            'best_dice',
+            'custom'
+        ]:
+            raise ValueError(f'unknown method name "{method_name}"')
+        
+        if method_name == 'custom':
+            self._alpha_q = alpha_q
+            self._alpha_w = alpha_w
+            self._alpha_r = alpha_r
+            self._w_positivity = w_positivity
+            self._enable_lambda = enable_lambda
+        else:
+            self._alpha_q = float(method_name in ['gen_dice', 'gradient_dice'])
+            self._alpha_w = float(method_name in ['dual_dice', 'best_dice'])
+            self._alpha_r = method_name in ['mql', 'best_dice']
+            self._w_positivity = method_name in ['gen_dice', 'best_dice']
+            self._enable_lambda = method_name in ['gen_dice', 'gradient_dice', 'best_dice']
+
+        self._q_function = q_function.to(self._device)
+        self._w_function = w_function.to(self._device)
+
+        self._q_lr = q_lr
+        self._w_lr = w_lr
+        self._lambda_lr = lambda_lr
+
+        self._gamma = gamma
+        self._f1_function = f1_function
+        self._f2_function = f2_function
+
+        self._q_optimizer = Adam(self._q_function.parameters(), lr=self._q_lr)
+        self._w_optimizer = Adam(self._w_function.parameters(), lr=self._w_lr, maximize=True)
+
+        if self._enable_lambda:
+            self._lambda = torch.ones((1,), device=self._device, requires_grad=True)
+            self._lambda_optimizer = Adam([self._lambda], lr=self._lambda_lr)
+        else:
+            self._lambda = torch.zeros((1,), device=self._device)
+
+        self._reset_experiment_data()
+
+
+    def _reset_experiment_data(self):
+        self._experiment_data = {
+            'initial_loss' : [],
+            'td_loss' : [],
+            'q_reg_loss' : [],
+            'w_reg_loss' : [],
+            'loss' : [],
+            'value_per_step' : []
+        }
+
+    def _write_experiment_data(self, experiment_name: str):
+        exp_folder = Path(f"experiments/{experiment_name}")
+        exp_folder.mkdir(parents=True, exist_ok=True)
+
+        params = {
+            'q_lr' : self._q_lr,
+            'w_lr' : self._w_lr,
+            'lambda_lr' : self._lambda_lr,
+            'gamma' : self._gamma,
+            'alpha_q' : self._alpha_q,
+            'alpha_w' : self._alpha_w,
+            'alpha_r' : self._alpha_r,
+            'w_positivity' : self._w_positivity,
+            'enable_lambda' : self._enable_lambda,
+            'f1_function' : get_lambda_code(self._f1_function),
+            'f2_function' : get_lambda_code(self._f2_function),
+            'seed' : self._seed
+        }
+
+        with open(exp_folder / "parameters.json", 'w') as f:
+            json.dump(params, f, indent="\t")
+
+        sns.set_theme()
+        plt.title("loss")
+        plt.plot(self._experiment_data['initial_loss'], alpha=0.7, label='initial q')
+        plt.plot(self._experiment_data['td_loss'], alpha=0.7, label='td')
+        plt.plot(self._experiment_data['q_reg_loss'], alpha=0.7, label='q reg')
+        plt.plot(self._experiment_data['w_reg_loss'], alpha=0.7, label='w reg')
+        plt.plot(self._experiment_data['loss'], alpha=0.7, label='loss')
+        plt.legend()
+        plt.savefig(exp_folder / "losses.pdf", bbox_inches="tight")
+        plt.close()
+
+        sns.set_theme()
+        plt.title(f"values")
+        plt.plot(self._experiment_data['value_per_step'])
+        plt.savefig(exp_folder / "values.pdf", bbox_inches="tight")
+        plt.close()
+
+    def _report_loss(
+        self,
+        initial: float,
+        td: float,
+        q_reg: float,
+        w_reg: float,
+        loss: float
+    ):
+        self._experiment_data['initial_loss'].append(initial)
+        self._experiment_data['td_loss'].append(td)
+        self._experiment_data['q_reg_loss'].append(q_reg)
+        self._experiment_data['w_reg_loss'].append(w_reg)
+        self._experiment_data['loss'].append(loss)
+
+    def _report_value(self, value: float):
+        self._experiment_data['value_per_step'].append(value)
+
+
+    def objective_function(
+        self,
+        first_state: torch.Tensor,
+        first_action: torch.Tensor,
+        state: torch.Tensor,
+        action: torch.Tensor,
+        reward: torch.Tensor,
+        next_state: torch.Tensor,
+        next_action: torch.Tensor
+    ):
+        initial_value = (1 - self._gamma) * self._q_function(
+            first_state, first_action
+        ).mean() + self._lambda
+
+        q = self._q_function(state, action)
+        w = self._w_function(state, action)
+
+        if self._w_positivity:
+            w = w**2
+
+        td_loss = (
+            w * (
+                self._alpha_r * reward
+                + self._gamma * self._q_function(next_state, next_action)
+                - q
+                - self._lambda
+            )
+        ).mean()
+
+        q_regularization = self._alpha_q * self._f1_function(q).mean()
+        w_regularization = self._alpha_w * self._f2_function(w).mean()
+
+        return initial_value, td_loss, q_regularization, w_regularization
+    
+    def fit(
+        self,
+        state: List[np.ndarray],
+        action: List[np.ndarray],
+        reward: List[np.ndarray],
+        target_action: List[np.ndarray],
+        num_steps: int,
+        batch_size: int = 1024,
+        eval_iter: int = 100,
+        result_folder: str = None
+    ):
+        check_scalar(num_steps, name='num_steps', target_type=int, min_val=1)
+        check_scalar(batch_size, name='batch_size', target_type=int, min_val=1)
+
+        if not (
+            len(state)
+            == len(action)
+            == len(reward)
+            == len(target_action)
+        ):
+            raise ValueError('number of trajectories mismatch')
+        
+        num_trajectories = len(state)
+
+        for i in range(num_trajectories):
+            check_array(state[i], name=f'state[{i}]', expected_dim=2)
+            check_array(action[i], name=f'action[{i}]', expected_dim=1)
+            check_array(reward[i], name=f'reward[{i}]', expected_dim=1)
+            check_array(target_action[i], name=f'target_action[{i}]', expected_dim=1)
+
+            if not (
+                state[i].shape[0]
+                == action[i].shape[0]
+                == reward[i].shape[0]
+                == target_action[i].shape[0]
+            ):
+                raise ValueError(f'trajectory length mismatch at index {i}')
+            
+        self._reset_experiment_data()
+
+        trajectory_len = [len(r) for r in reward]
+        trajectory_offset = np.cumsum([0] + trajectory_len)[:-1]
+
+        trajectory_len = torch.tensor(
+            trajectory_len, dtype=torch.long, device=self._device
+        )
+        trajectory_offset = torch.tensor(
+            trajectory_offset, dtype=torch.long, device=self._device
+        )
+
+        state_tensor = torch.tensor(
+            np.concatenate(state), dtype=torch.float, device=self._device
+        )
+        action_tensor = torch.tensor(
+            np.concatenate(action), dtype=torch.long, device=self._device
+        )
+        reward_tensor = torch.tensor(
+            np.concatenate(reward), dtype=torch.float, device=self._device
+        )
+        target_action_tensor = torch.tensor(
+            np.concatenate(target_action), dtype=torch.long, device=self._device
+        )
+
+        tqdm_iterator = tqdm(range(num_steps), total=num_steps)
+        for i in tqdm_iterator:
+            idx = torch.randint(
+                num_trajectories,
+                size=(batch_size,),
+                device=self._device,
+                generator=self._torch_generator
+            )
+            t = torch.tensor([
+                torch.randint(
+                    0, l - 2, size=(1,),
+                    generator=self._torch_generator
+                )
+                for l in trajectory_len[idx]
+            ], dtype=torch.long, device=self._device)
+
+            flatten_idx = trajectory_offset[idx]
+            flatten_t = flatten_idx + t
+
+            initial_, td_, q_, w_ = self.objective_function(
+                first_state=state_tensor[flatten_idx],
+                first_action=target_action_tensor[flatten_idx],
+                state=state_tensor[flatten_t],
+                action=action_tensor[flatten_t],
+                reward=reward_tensor[flatten_t],
+                next_state=state_tensor[flatten_t + 1],
+                next_action=target_action_tensor[flatten_t + 1]
+            )
+            loss = initial_ + td_ + q_ - w_
+
+            self._q_optimizer.zero_grad()
+            self._w_optimizer.zero_grad()
+            if self._enable_lambda:
+                self._lambda_optimizer.zero_grad()
+
+            loss.backward()
+
+            clip_grad_norm_(self._q_function.parameters(), max_norm=10.0)
+            clip_grad_norm_(self._w_function.parameters(), max_norm=10.0)
+            if self._enable_lambda:
+                clip_grad_norm_(self._lambda, max_norm=10.0)
+
+            self._q_optimizer.step()
+            self._w_optimizer.step()
+            if self._enable_lambda:
+                self._lambda_optimizer.step()
+
+            if i % eval_iter == 0 or i == (num_steps - 1):
+                value = self.predict_per_step_reward(
+                    state=state,
+                    action=action,
+                    reward=reward
+                )
+
+                if result_folder is not None:
+                    self._report_value(value)
+
+            tqdm_iterator.set_description(
+                f'loss: {loss.item():.4f}; value: {value:.4f}'
+            )
+
+            if result_folder is not None:
+                self._report_loss(
+                    initial_.item(), td_.item(),
+                    q_.item(), w_.item(), loss.item()
+                )
+
+        if result_folder is not None:
+            self._write_experiment_data(result_folder)
+
+
+    @torch.no_grad()
+    def predict_weights(self, state: np.ndarray, action: np.ndarray):
+        check_array(state, name="state", expected_dim=2)
+        check_array(action, name="action", expected_dim=1)
+
+        if state.shape[0] != action.shape[0]:
+            raise ValueError("len(states) != len(actions)")
+        
+        state = torch.tensor(state, dtype=torch.float, device=self._device)
+        action = torch.tensor(action, dtype=torch.long, device=self._device)
+
+        w = self._w_function(state, action)
+
+        if self._w_positivity:
+            w = w**2
+
+        return w.to('cpu').detach().numpy()
+    
+    def predict_per_step_reward(
+        self,
+        state: List[np.ndarray],
+        action: List[np.ndarray],
+        reward: List[np.ndarray]
+    ):
+        w = self.predict_weights(
+            np.concatenate(state),
+            np.concatenate(action)
+        )
+
+        r = np.concatenate(reward)
+
+        return (w * r).mean().item()
+
+    def predict_per_traj_reward(
+        self,
+        state: List[np.ndarray],
+        action: List[np.ndarray],
+        reward: List[np.ndarray]
+    ):
+        per_step_rewards = self.predict_per_step_reward(
+            state=state,
+            action=action,
+            reward=reward
+        )
+
+        total_steps = sum([s.shape[0] for s in state])
+
+        return (total_steps / len(state)) * per_step_rewards
---
+++ functions.py
@@ -0,0 +1,138 @@
+from enum import Enum
+
+import torch
+import torch.nn as nn
+from pytorch_revgrad import RevGrad
+
+from sklearn.utils import check_scalar
+
+
+class DiceFunctions(Enum):
+    P_2                 = lambda x: x ** 2 / 2
+    P_3                 = lambda x: torch.abs(x) ** 3 / 3
+    DUAL_DICE_P_3_2     = lambda x: torch.abs(x) ** 1.5 / 1.5
+    SCOPE_RL_P_2        = lambda x: x ** 2
+    CHI_SQUARED         = lambda x: (x - 1) ** 2 / 2
+    NEYMAN_CHI_SQUARED  = lambda x: (x - 1) ** 2
+    PEARSON_CHI_SQUARED = lambda x: (x - 1) ** 2 / x
+    KL_DIVERGENCE       = lambda x: x * torch.log(torch.abs(x))
+
+
+class StateActionNetwork(nn.Module):
+    def __init__(
+        self,
+        state_dim: int,
+        hidden_dim: int,
+        action_dim: int,
+        num_layers: int,
+        seed: int = None
+    ) -> None:
+        super().__init__()
+
+        check_scalar(num_layers, name='num_layers', target_type=int, min_val=2)
+
+        self._torch_generator = torch.Generator()
+        if seed is not None:
+            self._torch_generator.manual_seed(seed)
+        else:
+            self._torch_generator.seed()
+
+    def init_weights(self):
+        for _, param in self.named_parameters():
+            try:
+                torch.nn.init.xavier_uniform_(
+                    param.data, generator=self._torch_generator
+                )
+            except:
+                torch.nn.init.normal_(
+                    param.data, generator=self._torch_generator
+                )
+
+    def forward(self, states: torch.Tensor, actions: torch.Tensor):
+        raise NotImplementedError()
+
+
+class DiscreteSANetwork(StateActionNetwork):
+    def __init__(
+        self,
+        state_dim: int,
+        hidden_dim: int,
+        action_dim: int,
+        num_layers: int,
+        action_emb: torch.Tensor = None,
+        seed: int = None,
+        device: str = 'cpu'
+    ):
+        super().__init__(
+            state_dim=state_dim,
+            hidden_dim=hidden_dim,
+            action_dim=action_dim,
+            num_layers=num_layers,
+            seed=seed
+        )
+
+        if action_emb is None:
+            action_emb = torch.eye(action_dim)
+
+        self._action_emb = action_emb.to(device)
+
+        layers = [
+            nn.Linear(state_dim + action_dim, hidden_dim),
+            nn.ReLU()
+        ]
+
+        for _ in range(num_layers - 2):
+            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(hidden_dim, 1))
+
+        self._network = nn.Sequential(*layers)
+
+        self.init_weights()
+
+    def forward(self, states: torch.Tensor, actions: torch.Tensor):
+        s_a = torch.cat([
+            states,
+            self._action_emb[actions]
+        ], dim=1)
+
+        return self._network(s_a).flatten()
+
+
+class MultiheadDiscreteSANetwork(StateActionNetwork):
+    def __init__(
+        self,
+        state_dim: int,
+        hidden_dim: int,
+        action_dim: int,
+        num_layers: int,
+        seed: int = None
+    ):
+        super().__init__(
+            state_dim=state_dim,
+            hidden_dim=hidden_dim,
+            action_dim=action_dim,
+            num_layers=num_layers,
+            seed=seed
+        )
+
+        layers = [
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU()
+        ]
+
+        for _ in range(num_layers - 2):
+            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(hidden_dim, action_dim))
+
+        self._network = nn.Sequential(*layers)
+
+        self.init_weights()
+
+    def forward(self, states: torch.Tensor, actions: torch.Tensor):
+        values = self._network(states)
+
+        return values.gather(1, actions.view(-1, 1)).flatten()
